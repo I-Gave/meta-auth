d8cbf5f6f460e9dbd3a92984acd88bded0a61aa0
tenthirtyone <Spactigus@gmail.com>
2018-02-24 18:01:39 -0500

    Defaults and license

--- index.js
+++ index.js
@@ -5,16 +5,33 @@ const uuidv4 = require('uuid/v4');
 const crypto = require('crypto');
 
 const secret = uuidv4();
-const cache = new NodeCache({
+let cache = new NodeCache({
   stdTTL: 600
 });
 
 function metaAuth(options) {
   return function(req, res, next) {
 
+    const DEFAULT_OPTIONS = {
+      message: 'MetaMessage',
+      signature: 'MetaSignature',
+      address: 'MetaAddress'
+    }
+
+    if (options.stdTTL)  {
+      cache = new NodeCache({
+        StdTTL: options.stdTTL
+      })
+    }
+
+    this.options = Object.assign(
+      DEFAULT_OPTIONS,
+      options
+    )
+
     // Address param is passed & isValidAddress
-    if (req.params[options.address]) {
-      const address = req.params[options.address];
+    if (req.params[this.options.address]) {
+      const address = req.params[this.options.address];
 
       if (ethUtil.isValidAddress(address)) {
         const challenge = createChallenge(address);
@@ -26,11 +43,11 @@ function metaAuth(options) {
     }
 
     // Challenge message returned with signature
-    if (req.params[options.message] &&
-        req.params[options.signature]) {
+    if (req.params[this.options.message] &&
+        req.params[this.options.signature]) {
       const recovered = checkChallenge(
-        req.params[options.message],
-        req.params[options.signature]
+        req.params[this.options.message],
+        req.params[this.options.signature]
       )
       let token = {
         recovered
@@ -54,23 +71,23 @@ function createChallenge (address) {
     `, 'utf8'));
 
   cache.set(address, challenge);
-  console.log(challenge)
+
   return challenge;
 }
 
 function checkChallenge(data, sig) {
-  let recovered = null;
-  try {
-    recovered = sigUtil.recoverPersonalSignature({
-      data,
-      sig
-    });
-  } catch (e) {
-    console.error(e);
+  const recovered = sigUtil.recoverPersonalSignature({
+    data,
+    sig
+  });
+
+  const challenge = cache.get(recovered);
+
+  if (challenge === data) {
     return recovered;
   }
 
-  return recovered;
+  return null;
 }
 module.exports = metaAuth;
 
