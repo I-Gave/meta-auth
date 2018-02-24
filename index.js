a3946718bfa3b51dc84eb70fb1508a3cc02c42b3
tenthirtyone <Spactigus@gmail.com>
2018-02-24 11:50:41 -0500

    add readme, uuid, option matching

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+const ethUtil = require('ethereumjs-util');
+const sigUtil = require('eth-sig-util');
+const uuidv4 = require('uuid/v4');
 
+const secret = uuidv4();
 
 function metaAuth(options) {
   return function(req, res, next) {
     console.log(options);
+
+    if (req.params[options.address]) {
+      console.log("Found Address");
+    }
+    if (req.params[options.message] &&
+        req.params[options.signature]) {
+      console.log("Found msg and sig");
+    }
+
+
     next();
   };
 }
 
+
 module.exports = metaAuth;
+
