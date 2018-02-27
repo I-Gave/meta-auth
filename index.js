386d56b7766260ad71fae671816c3094fa04268f
tenthirtyone <Spactigus@gmail.com>
2018-02-26 19:53:06 -0500

    updated example

--- index.js
+++ index.js
@@ -9,88 +9,83 @@ let cache = new NodeCache({
   stdTTL: 600
 });
 
-function metaAuth(options) {
-  return function(req, res, next) {
+class MetaAuth {
+  constructor(options) {
+    return (req, res, next) => {
+      const DEFAULT_OPTIONS = {
+        signature: 'MetaSignature',
+        message: 'MetaMessage',
+        address: 'MetaAddress'
+      }
 
-    const DEFAULT_OPTIONS = {
-      signature: 'MetaSignature',
-      message: 'MetaMessage',
-      address: 'MetaAddress'
-    }
+      this.options = Object.assign(
+        DEFAULT_OPTIONS,
+        options
+      )
 
-    this.options = Object.assign(
-      DEFAULT_OPTIONS,
-      options
-    )
+      // Address param is passed & isValidAddress
+      if (req.params[this.options.address]) {
+        const address = req.params[this.options.address];
 
-    // Address param is passed & isValidAddress
-    if (req.params[this.options.address]) {
-      const address = req.params[this.options.address];
+        if (ethUtil.isValidAddress(address)) {
+          const challenge = this.createChallenge(address);
+          let token = {
+            challenge
+          }
+          req.metaAuth = token;
+        }
+      }
 
-      if (ethUtil.isValidAddress(address)) {
-        const challenge = createChallenge(address);
+      // Challenge message returned with signature
+      if (req.params[this.options.message] &&
+        req.params[this.options.signature]) {
+
+        const recovered = this.checkChallenge(
+          req.params[this.options.message],
+          req.params[this.options.signature]
+        )
         let token = {
-          challenge
+          recovered
         }
         req.metaAuth = token;
       }
+
+      next();
     }
-
-    // Challenge message returned with signature
-    if (req.params[this.options.message] &&
-        req.params[this.options.signature]) {
-
-      const recovered = checkChallenge(
-        req.params[this.options.message],
-        req.params[this.options.signature]
-      )
-      let token = {
-        recovered
-      }
-      req.metaAuth = token;
-    }
-
-    next();
-  };
-}
-
-function createChallenge (address) {
-  const hash = crypto.createHmac('sha256', secret)
-    .update(address + uuidv4())
-    .digest('hex');
-
-  const challenge = [{
-    type: 'string',
-    name: 'challenge',
-    value: hash
-  }];
-
-  cache.set(address, challenge);
-
-  return challenge;
-}
-
-function checkChallenge(challenge, sig) {
-  const data = [{
-    type: 'string',
-    name: 'challenge',
-    value: challenge
-  }]
-  const recovered = sigUtil.recoverTypedSignature({
-    data,
-    sig
-  });
-
-  const storedChallenge = cache.get(recovered);
-
-
-  if (storedChallenge[0].value === challenge) {
-    cache.del(recovered);
-    return recovered;
   }
 
-  return false;
+  createChallenge(address) {
+    const hash = crypto.createHmac('sha256', secret)
+      .update(address + uuidv4())
+      .digest('hex');
+
+    cache.set(address, hash);
+
+    return hash;
+  }
+
+  checkChallenge(challenge, sig) {
+    const data = [{
+      type: 'string',
+      name: 'challenge',
+      value: challenge
+    }]
+    const recovered = sigUtil.recoverTypedSignature({
+      data,
+      sig
+    });
+
+    const storedChallenge = cache.get(recovered);
+
+    if (storedChallenge === challenge) {
+      cache.del(recovered);
+      return recovered;
+    }
+
+    return false;
+  }
 }
 
-module.exports = metaAuth;
+
+module.exports = MetaAuth;
 
